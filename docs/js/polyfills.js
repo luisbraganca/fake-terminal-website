670ae34ffa5da70e9a833fcedd67bf2336312f3f
Luís Bragança <luisbraganca@protonmail.com>
2018-03-01 11:42:43 +0000

    Added 'doc' folder for a preview

---
+++ docs/js/polyfills.js
@@ -0,0 +1,9 @@
+/**
+ * Add startsWith support for IE
+ */
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
